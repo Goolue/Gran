1a937fe044a819ffe58bbf5eae5ae808bf3fd1f8
Oded <oded.rojtblat@gmail.com>
2018-11-21 17:23:45 +0200

    clean

--- Source/PlayComponent.cpp
+++ Source/PlayComponent.cpp
@@ -71,7 +71,6 @@ void PlayComponent::run() {
             addBuffersToQueue();
         }
         checkForBuffersToFree();
-//        wait(100);
     }
 }
 
@@ -86,38 +85,6 @@ void PlayComponent::checkForBuffersToFree() {
 
 void PlayComponent::getNextAudioBlock(const AudioSourceChannelInfo& bufferToFill) {
     if (fileLoaded && state == Play && !buffersQueue.empty()) {
-//        ReferenceCountedBuffer::Ptr retainedCurrentBuffer(currentBuffer);
-//        if (retainedCurrentBuffer == nullptr) {
-//            bufferToFill.clearActiveBufferRegion();
-//            return;
-//        }
-//        auto* currentAudioSampleBuffer = retainedCurrentBuffer->getAudioSampleBuffer();
-//        auto position = retainedCurrentBuffer->position;
-//        auto numInputChannels = currentAudioSampleBuffer->getNumChannels();
-//        auto numOutputChannels = bufferToFill.buffer->getNumChannels();
-//        auto outputSamplesRemaining = bufferToFill.numSamples;
-//        auto outputSamplesOffset = 0;
-//        while (outputSamplesRemaining > 0) {
-//            auto bufferSamplesRemaining = currentAudioSampleBuffer->getNumSamples() - position;
-//            auto samplesThisTime = jmin(outputSamplesRemaining, bufferSamplesRemaining);
-//            for (auto channel = 0; channel < numOutputChannels; ++channel) {
-//                bufferToFill.buffer->copyFrom(channel,
-//                                              bufferToFill.startSample + outputSamplesOffset,
-//                                              *currentAudioSampleBuffer,
-//                                              channel % numInputChannels,
-//                                              position,
-//                                              samplesThisTime);
-//            }
-//            outputSamplesRemaining -= samplesThisTime;
-//            outputSamplesOffset += samplesThisTime;
-//            position += samplesThisTime;
-//            if (position == currentAudioSampleBuffer->getNumSamples()) {
-//                position = 0;
-//            }
-//        }
-//        retainedCurrentBuffer->position = position;
-
-
         auto retainedCurrentBuffer = getAudioBufferFromQueue();
         if (retainedCurrentBuffer == nullptr) {
             cerr << "queue is empty!" << endl;
@@ -126,21 +93,14 @@ void PlayComponent::getNextAudioBlock(const AudioSourceChannelInfo& bufferToFill
         }
         auto* currentAudioSampleBuffer = retainedCurrentBuffer->getAudioSampleBuffer();
         auto position = retainedCurrentBuffer->position;
-        cout << "buff from queue, position now " << position << endl;
         auto numInputChannels = currentAudioSampleBuffer->getNumChannels();
         auto numOutputChannels = bufferToFill.buffer->getNumChannels();
         auto outputSamplesRemaining = bufferToFill.numSamples;
-        cout << "bufferToFill size " << outputSamplesRemaining << endl;
         auto outputSamplesOffset = 0;
         while (outputSamplesRemaining > 0) {
             auto bufferSamplesRemaining = currentAudioSampleBuffer->getNumSamples() - position;
             auto samplesThisTime = jmin(outputSamplesRemaining, bufferSamplesRemaining);
-            cout << "samples this time " << samplesThisTime << endl;
             for (auto channel = 0; channel < numOutputChannels; ++channel) {
-                cout << "copy: dest start " << bufferToFill.startSample + outputSamplesOffset <<
-                     ", dest end " << bufferToFill.startSample + outputSamplesOffset + samplesThisTime <<
-                     ", dest size " << bufferToFill.buffer->getNumSamples() <<
-                     ", outputSamplesRemaining " << outputSamplesRemaining << endl;
                 bufferToFill.buffer->copyFrom(channel,
                                               bufferToFill.startSample + outputSamplesOffset,
                                               *currentAudioSampleBuffer,
@@ -152,66 +112,19 @@ void PlayComponent::getNextAudioBlock(const AudioSourceChannelInfo& bufferToFill
             outputSamplesOffset += samplesThisTime;
             position += samplesThisTime;
             if (position == currentAudioSampleBuffer->getNumSamples()) {
-                cout << "popping new arr, position now " << position << " ref count now " <<
-                     retainedCurrentBuffer->getReferenceCount() << endl;
                 buffersQueue.pop_front();
-                cout << "after pop ref count now " << retainedCurrentBuffer->getReferenceCount() << endl;
                 retainedCurrentBuffer = getAudioBufferFromQueue();
                 if (retainedCurrentBuffer == nullptr) {
-                    cerr << "queue is empty! in while" << endl;
                     bufferToFill.clearActiveBufferRegion();
                     return;
                 }
                 currentAudioSampleBuffer = retainedCurrentBuffer->getAudioSampleBuffer();
                 position = retainedCurrentBuffer->position;
-                cout << "new position now " << position << endl;
             }
         }
         cout << "setting position " << position << endl;
         retainedCurrentBuffer->position = position;
 
-//        auto* currentAudioSampleBuffer = getAudioBufferFromQueue();
-//        if (currentAudioSampleBuffer == nullptr) {
-//            cerr << "queue is empty!" << endl;
-//            bufferToFill.clearActiveBufferRegion();
-//            return;
-//        }
-//        int position = 0;
-//        auto numOutputChannels = bufferToFill.buffer->getNumChannels();
-//        auto outputSamplesRemaining = bufferToFill.numSamples;
-//        auto numInputChannels = currentAudioSampleBuffer->getNumChannels();
-//        auto outputSamplesOffset = 0;
-//        while (outputSamplesRemaining > 0) {
-//            auto bufferSamplesRemaining = currentAudioSampleBuffer->getNumSamples() - position;
-//            auto samplesThisTime = jmin(outputSamplesRemaining, bufferSamplesRemaining);
-//            cout << "samples this time " << samplesThisTime << " bufferSamplesRemaining " << bufferSamplesRemaining
-//                 << " outputSamplesRemaining " << outputSamplesRemaining << " num samples " <<
-//                 currentAudioSampleBuffer->getNumSamples() << endl;
-//            for (auto channel = 0; channel < numOutputChannels; ++channel) {
-//                cout << "copying channel " << channel << endl;
-//                bufferToFill.buffer->copyFrom(channel,
-//                                              bufferToFill.startSample + outputSamplesOffset,
-//                                              *currentAudioSampleBuffer,
-//                                              channel % numInputChannels,
-//                                              position,
-//                                              samplesThisTime);
-//                cout << "copied channel " << channel << endl;
-//            }
-//            outputSamplesRemaining -= samplesThisTime;
-//            outputSamplesOffset += samplesThisTime;
-//            position += samplesThisTime;
-//            if (position == currentAudioSampleBuffer->getNumSamples()) {
-//                cout << "reseting position" << endl;
-//                position = 0;
-//                outputSamplesOffset = 0;
-//                currentAudioSampleBuffer = getAudioBufferFromQueue();
-//                if (currentAudioSampleBuffer == nullptr) {
-//                    cerr << "queue is empty! inside while" << endl;
-//                    bufferToFill.clearActiveBufferRegion();
-//                    return;
-//                }
-//            }
-//        }
     } else {
         bufferToFill.clearActiveBufferRegion();
     }
@@ -222,11 +135,7 @@ ReferenceCountedBuffer::Ptr PlayComponent::getAudioBufferFromQueue() {
     if (buffFromQueue == nullptr) {
         return nullptr;
     }
-//    buffersQueue.pop_front();
-    cout << "popped from queue. queue size is now " << buffersQueue.size() << endl;
     return buffFromQueue;
-//    auto* currentAudioSampleBuffer = buffFromQueue->getAudioSampleBuffer();
-//    return currentAudioSampleBuffer;
 }
 
 void PlayComponent::prepareToPlay(int samplesPerBlockExpected, double sampleRate) {
@@ -237,7 +146,6 @@ void PlayComponent::prepareToPlay(int samplesPerBlockExpected, double sampleRate
 
 void PlayComponent::addBuffersToQueue() {
     if (fileLoaded && buffersQueue.size() < MAX_QUEUE_SIZE) {
-        cout << "starting add to queue" << endl;
         ReferenceCountedBuffer::Ptr retainedCurrentBuffer(currentBuffer);
         if (retainedCurrentBuffer == nullptr) {
             cout << "retained buffer is nullptr" << endl;
@@ -247,7 +155,6 @@ void PlayComponent::addBuffersToQueue() {
         auto position = retainedCurrentBuffer->position;
 
         while (buffersQueue.size() < MAX_QUEUE_SIZE) {
-            cout << "starting push to queue. queue size is now " << buffersQueue.size() << endl;
 
             ReferenceCountedBuffer::Ptr buffToPush = new ReferenceCountedBuffer("buffToPush" + String(currBuffIndex),
                                                                                 numOfChannels,
@@ -259,14 +166,7 @@ void PlayComponent::addBuffersToQueue() {
             while (outputSamplesOffset < audioSampleBufferToPush->getNumSamples()) {
                 auto bufferSamplesRemaining = currentAudioSampleBuffer->getNumSamples() - position;
                 auto samplesThisTime = jmin(samplesPerBlockExpected - outputSamplesOffset, bufferSamplesRemaining);
-                cout << "push samples this time " << samplesThisTime <<
-                    " push bufferSamplesRemaining " << bufferSamplesRemaining
-                     << " push outputSamplesOffset " << outputSamplesOffset << " push num samples " <<
-                     currentAudioSampleBuffer->getNumSamples() << endl;
                 for (int channel = 0; channel < numOfChannels; channel++) {
-                    cout << "push copy: dest start " << outputSamplesOffset <<
-                         ", dest end " << samplesThisTime + outputSamplesOffset  <<
-                         ", dest size " << audioSampleBufferToPush->getNumSamples() << endl;
                     audioSampleBufferToPush->copyFrom(channel, outputSamplesOffset, *currentAudioSampleBuffer,
                                                       channel, position, samplesThisTime);
                 }
